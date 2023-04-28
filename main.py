15e95d94319793c66a6d57e963b46adf976df943
Abdelatif Hamdi <abdelatifhamdi702@gmail.com>
2023-04-28 11:27:27 +0100

    CORS Problem

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ import tensorflow as tf
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-    "https://weat-leaf-diseases-detection-website.vercel.app/",
-]
+#origins = [
+#    "http://localhost",
+#    "http://localhost:3000",
+#   "https://weat-leaf-diseases-detection-website.vercel.app/",
+#]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
